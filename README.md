983ed0a08bf571de18acfcb39b14953441a2eb2c
YusukeIwaki <yusuke-iwaki@example.com>
2019-11-21 01:36:05 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# ggrks-puppeteer-dart
+# [puppeteerのDart版](https://pub.dev/packages/puppeteer) とお手持ちのChrome for Macを使って自動操作をするテンプレ
+
+https://github.com/YusukeIwaki/ggrks-puppeteer-js のDart版。
+
+dart2nativeでワンバイナリに。ggrksコマンド for Macが爆誕ｗｗ
+
+![ggrks](https://user-images.githubusercontent.com/11763113/69257935-2dbabc00-0bff-11ea-8c83-3ff5006b302a.gif)
---
+++ ggrks.dart
@@ -0,0 +1,63 @@
+import 'dart:io';
+
+import 'package:puppeteer/puppeteer.dart';
+
+extension IterableExtension<T> on Iterable<T> {
+  firstOrNull() {
+    if (this.isEmpty) {
+      return null;
+    } else {
+      return this.first;
+    }
+  }
+}
+
+extension StringExtension on String {
+  ifEmpty(String defaultValue) {
+    if (this.isEmpty) {
+      return defaultValue;
+    } else {
+      return this;
+    }
+  }
+}
+
+main(List<String> args) async {
+  final String searchKeyword = args.join(" ").ifEmpty("puppeteer");
+
+  final browser = await puppeteer.launch(
+    // MacにインストールされているChromeを使う。
+    executablePath:
+        '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+
+    // ブラウザ画面を表示しながら（ヘッドレスモードを無効にする）。
+    headless: false,
+
+    args: [
+      // ゲストセッションで操作する。
+      "--guest",
+
+      // ウインドウサイズをデフォルトより大きめに。
+      '--window-size=1280,800',
+    ],
+
+    // 人間味のある速度で入力/操作する。
+    slowMo: Duration(milliseconds: 50),
+  );
+
+  // 大抵のサンプルコードはここで単純に browser.newPage() しているだけのものが多いが、
+  // ブラウザを開いたときにすでに１つタブが開いている場合には、２つ目のタブが開いてしまう。
+  // それを防ぐため、すでにタブが開いている場合にはそれを使うようにする。
+  final Page page =
+      (await browser.pages).firstOrNull() ?? (await browser.newPage());
+
+  await page.setViewport(DeviceViewport(width: 1280, height: 800));
+
+  await page.goto("https://google.com/");
+  await page.type("input[name='q']", searchKeyword);
+  await page.keyboard.press(Key.enter);
+
+  // 3秒後に閉じる。
+  await Future.delayed(Duration(seconds: 3));
+  await browser.close();
+}
